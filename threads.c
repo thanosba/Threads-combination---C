5e0373d2c0a9b6303a4837e68ad95834d4fe5801
Athanasios Balanikas <athanasios.balanikas@yahoo.gr>
2017-10-02 22:51:24 +0300

    Update threads.c

--- threads.c
+++ threads.c
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: thanosbalas
- *
- * Created on 2 Οκτωβρίου 2017, 10:31 μμ
- */
-
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
