dec53ff422dc0540ecd102a1efd2951c8a9bbfb4
Athanasios Balanikas <athanasios.balanikas@yahoo.gr>
2017-10-02 22:50:45 +0300

    Add files via upload

---
+++ threads.c
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: thanosbalas
+ *
+ * Created on 2 Οκτωβρίου 2017, 10:31 μμ
+ */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include "pthread.h"
+
+void * process(void * arg)
+{
+    int i;
+    fprint(stderr, "Start the thread %s\n", (char*)arg);
+    for(i=0;i<10;i++){
+        write(1, (char*)arg, 1);
+    }
+    return NULL;
+}
+int main()
+{
+    int retcode;
+    pthread_t th_a, th_b;
+    void * retval;
+    retcode = pthread_create(&th_a, NULL, process, "a");
+    if (retcode !=0)fprintf(stderr, "create a failed%d\n"retcode);
+    retcode = pthread_create(&th_b, NULL, process, "b");
+    if (retcode !=0)fprintf(stderr, "create b failed%d\n"retcode);
+    retcode = pthread_join(th_a, &retval);
+    if (retcode !=0)fprintf(stderr, "join a failed%d\n"retcode);
+    retcode = pthread_join(th_b, &retval);
+    if (retcode !=0)fprintf(stderr, "join b failed%d\n"retcode);
+    return 0;   
+ }
